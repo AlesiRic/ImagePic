eb5ad7625b14361fddec553196a3fca162c20a09
AlesiRic <alrivelardego@ittepic.edu.mx>
2018-05-24 22:58:29 -0600

    Commit one
    new archive

---
+++ ImagePic/ImagePic/Page1.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ImagePic
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+
+
+    {
+
+        
+        public Page1()
+        {
+            InitializeComponent();
+            //var embeddedImage = new Image { Source = ImageSource.FromResource("WorkingWithImages.beach.jpg") };
+        }
+    }
+}
---
+++ ImagePic/ImagePic/Page2.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ImagePic
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page2 : ContentPage
+    {
+        public String picURL="https://vignette.wikia.nocookie.net/finalfantasy/images/7/70/Kefka-boss-ffvi.png/revision/latest?cb=20101215194938";
+        public Page2()
+        {
+            InitializeComponent();
+            //
+            var webImage = new Image {  };
+            var buttonPic = new Button { };
+        }
+
+        private void PressMeButton_Clicked(object sender, EventArgs e)
+        {
+            (sender as Button).Text = "I was just clicked!";
+        }
+    }
+}
